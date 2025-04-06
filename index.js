c4ce1c58da84e4d2f2844e285248587406c95067
omodigital <omodigitallabs@gmail.com>
2025-04-07 02:55:17 +0530

    commit 2

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MySQL Connection Setup
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'joobhvgiob',
-    password: 'p-5zG3%05dEd',
-    database: 'product_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-
 connection.connect(err => {
     if (err) {
         console.error('Error connecting to MySQL:', err);
